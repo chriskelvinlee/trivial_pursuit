273434c37cb697fb2a6b87ca1013014e1539c4de
Michael Chen <michaelchen.harvard@gmail.com>
2011-12-06 16:09:45 -0500

    test() will run through method with all the questions

--- test.py
+++ test.py
@@ -1,15 +1,35 @@
-import nltk
-from urllib import urlopen
-from nltk.collocations import *
-url = "http://www.nytimes.com/2011/11/18/nyregion/protesters-clash-with-police-in-lower-manhattan.html"
-html = urlopen(url).read()
-raw = nltk.clean_html(html)
-tokens = nltk.word_tokenize(raw)
-text = nltk.Text(tokens)
-text.collocations() # THIS GENERATES BIGRAMS
-trigram_measures = nltk.collocations.TrigramAssocMeasures()
-finder = TrigramCollocationFinder.from_words(tokens)
-finder.apply_word_filter(lambda w: len(w) < 3)
-finder.apply_freq_filter(3)
-scored = finder.score_ngrams(trigram_measures.raw_freq)
-print(scored) # THIS PRINTS TRIGRAMS
+from questions import *
+from trivialpursuitfunctions import *
+
+#What other options should test take in?
+def test(questions = historyQuestions):
+    numberCorrect = 0;
+    for question in questions:
+        text = question[0]
+        choices = question[1]
+        correct = question[2]
+        result = compute_score(queryphrase=text, answers=choices)
+
+        bld = text + ": "
+        if getHighestResult(result, choices) == correct:
+            numberCorrect += 1
+            bld += "Correct"
+        else:
+            bld += "Incorrect"
+        print bld
+    print str(correct) + "/" + str(len(questions))
+
+def getHighestResult(result, choices):
+    highestConfidence = 0
+    highestChoice = ""
+    for r in result:
+        if result[r] > highestConfidence:
+            highestChoice = r
+            highestConfidence = result[r]
+    
+    index = 0
+    for c in choices:
+        if c == highestChoice:
+            break
+        index += 1
+    return index
